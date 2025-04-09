8469777fe7371e4c23e0c70bca42ba5cb1e371eb
Mun Hyunboo <ansgusqnwl@naver.com>
2025-04-09 21:24:40 +0900

    C# 추가

---
+++ Factory/FactoryApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+abstract class Animal {
+    public abstract string Speak();
+}
+
+class Dog : Animal {
+    public override string Speak() => "Woof!";
+}
+
+class Cat : Animal {
+    public override string Speak() => "Meow!";
+}
+
+class AnimalFactory {
+    public Animal CreateAnimal(string type) {
+        return type switch {
+            "dog" => new Dog(),
+            "cat" => new Cat(),
+            _ => null
+        };
+    }
+}
+
+class Program {
+    static void Main() {
+        AnimalFactory factory = new AnimalFactory();
+
+        Animal dog = factory.CreateAnimal("dog");
+        Animal cat = factory.CreateAnimal("cat");
+
+        Console.WriteLine($"Dog says: {dog.Speak()}");
+        Console.WriteLine($"Cat says: {cat.Speak()}");
+    }
+}
